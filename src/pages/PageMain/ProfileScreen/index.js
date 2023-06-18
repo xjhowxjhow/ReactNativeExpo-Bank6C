3423132360d2cbd7062bfad77aa151079e67ad84
Jhonatan Deni <jhow.1516@hotmail.com>
2023-06-17 21:49:24 -0300

    Update

--- src/pages/PageMain/ProfileScreen/index.js
+++ src/pages/PageMain/ProfileScreen/index.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function PageMain() {
+export default function ProfileScreen() {
+
+  
   return (
     <View>
-      <Text>Main PAGE</Text>
+      <Text>ProfileScreenPage</Text>
     </View>
   )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,46 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //importar as telas
 import Welcome from '../pages/Welcome'
-import PageMain from '../pages/PageMain'
+
 
 const Stack = createNativeStackNavigator() // Cria a pilha de navegação
 
+import HomeScreen from '../pages/PageMain/HomeScreen'
+import ProfileScreen from '../pages/PageMain/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+    );
+}
 
 
-export default function Routes(){
 
-    return(
+export default function Routes() {
+
+    return (
         <Stack.Navigator>
-            <Stack.Screen 
-                name="Welcome" 
-                component={Welcome} 
-                options={{headerShown:false}} // Oculta o cabeçalho
-                />
+            <Stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{ headerShown: false }} // Oculta o cabeçalho
+            />
 
-            <Stack.Screen 
-                name="PageMain" 
-                component={PageMain}
-                options={{headerShown:false}} // Oculta o cabeçalho
-                />
+            <Stack.Screen
+                name="PageMain"
+                component={MainTabs}
+                options={{ headerShown: false }}
+            />
+
+
+
 
         </Stack.Navigator>
 
